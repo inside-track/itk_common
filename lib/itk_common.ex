0990ac723fff6724e592fbf0fad1df7113341d46
Grady Griffin <grady.griffin@insidetrack.com>
2020-01-09 22:40:14 -0500

    first commit

---
+++ lib/itk_common.ex
@@ -0,0 +1,18 @@
+defmodule ItkCommon do
+  @moduledoc """
+  Documentation for ItkCommon.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ItkCommon.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/itk_common_test.exs
@@ -0,0 +1,8 @@
+defmodule ItkCommonTest do
+  use ExUnit.Case
+  doctest ItkCommon
+
+  test "greets the world" do
+    assert ItkCommon.hello() == :world
+  end
+end
