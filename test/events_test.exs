dc250af8930078762d82b3979490192fed1439db
Grady Griffin <grady.griffin@insidetrack.com>
2020-08-06 02:32:40 -0400

    BE-8 add events

---
+++ test/events_test.exs
@@ -0,0 +1,106 @@
+defmodule ITKCommon.EventsTest do
+  use ExUnit.Case, async: true
+
+  alias ITKCommon.Events
+  alias ITKCommon.UserSessions
+
+  describe "capture/2" do
+    test "publishes an event from token" do
+      user = %{uuid: "xyz", user_role: "student"}
+
+      token =
+        UserSessions.start(user, %{
+          "app_version" => "app-1.1",
+          "os_version" => "os-1.1",
+          "device_type" => "device-Awesome",
+          "ip" => "1.1.1.1",
+          "ip_location" => "US",
+          "organization_uuid" => "abc"
+        })
+
+      event_data = %{
+        "name" => "test_event",
+        "arbitrary_data_field" => "hello"
+      }
+
+      assert :ok = Events.capture(event_data, token)
+
+      assert_received [
+        :publish,
+        "event.capture",
+        %{
+          "name" => "test_event",
+          "user_uuid" => "xyz",
+          "user_role" => "student",
+          "organization_uuid" => "abc",
+          "session_id" => _,
+          "arbitrary_data_field" => "hello",
+          "app_version" => "app-1.1",
+          "os_version" => "os-1.1",
+          "device_type" => "device-Awesome",
+          "ip" => "1.1.1.1",
+          "ip_location" => "US",
+          "timestamp" => _
+        }
+      ]
+
+      UserSessions.terminate_all(user)
+    end
+
+    test "publishes an event with session data" do
+      session_data = %{
+        "uuid" => "xyz",
+        "role" => "student",
+        "session_id" => "session_id",
+        "organization_uuid" => "abc"
+      }
+
+      event_data = %{
+        "name" => "test_event",
+        "arbitrary_data_field" => "hello"
+      }
+
+      assert :ok = Events.capture(event_data, session_data)
+
+      assert_received [
+        :publish,
+        "event.capture",
+        %{
+          "user_uuid" => "xyz",
+          "user_role" => "student",
+          "organization_uuid" => "abc",
+          "session_id" => "session_id",
+          "arbitrary_data_field" => "hello",
+          "timestamp" => _
+        }
+      ]
+    end
+
+    test "publishes an event with student" do
+      user = %{uuid: "xyz", user_role: "student"}
+      UserSessions.start(user, %{"organization_uuid" => "abc"})
+
+      event_data = %{
+        "name" => "test_event",
+        "arbitrary_data_field" => "hello"
+      }
+
+      assert :ok = Events.capture(event_data, user)
+
+      assert_received [
+        :publish,
+        "event.capture",
+        %{
+          "user_uuid" => "xyz",
+          "user_role" => "student",
+          "organization_uuid" => "abc",
+          "session_id" => _,
+          "arbitrary_data_field" => "hello",
+          "timestamp" => _
+        }
+      ]
+
+      UserSessions.terminate_all(user)
+    end
+  end
+end
--- test/zip_geolocation_test.exs
+++ test/zip_geolocation_test.exs
@@ -1,5 +1,5 @@
 defmodule ITKCommon.ZipGeolocationTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
   alias ITKCommon.ZipGeolocation
   alias ITKCommon.Redis
@@ -13,7 +13,7 @@ defmodule ITKCommon.ZipGeolocationTest do
   describe "get/1" do
     test "Get geopoint" do
       assert Redis.hget(ZipGeolocation.key(), "loaded") == {:ok, nil}
-
+      Process.sleep(50)
       assert ZipGeolocation.get("00601") == {:ok, "18.180555, -66.749961"}
 
       assert ZipGeolocation.get("006020000") == {:ok, "18.361945, -67.175597"}
@@ -22,6 +22,7 @@ defmodule ITKCommon.ZipGeolocationTest do
     test "sets loaded flag" do
       assert Redis.hget(ZipGeolocation.key(), "loaded") == {:ok, nil}
       ZipGeolocation.load(%{})
+      Process.sleep(50)
       assert Redis.hget(ZipGeolocation.key(), "loaded") == {:ok, "true"}
     end
 
